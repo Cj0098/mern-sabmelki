6b37290a8546df04bce6940091d7e99096a594fb
Cj0098 <jj31021380@example.com>
2023-12-30 12:41:55 +0330

    sign up api & route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config()
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,12 +15,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
+app.use(express.json())
 
 app.listen(3000, ()=>{
-    console.log("runninasdasdg");
-
-
-    
+    console.log("runninasdasdg"); 
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
